86cafdad79b9e7ccef458a79907db6fc4ebbafda
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-13 11:31:05 +0530

    dao layout

---
+++ src/edu/ijse/layered/dao/DaoFactory.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.dao;
+
+/**
+ *
+ * @author anjanathrishakya
+ */
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+    
+    private DaoFactory(){}
+    
+    public static DaoFactory getInstance(){
+        if(daoFactory == null){
+            daoFactory = new DaoFactory();
+        }
+        return daoFactory;
+    }
+    
+    public SuderDao grtDao(DaoTypes type){
+        switch (type) {
+            case ITEM:
+               
+            default:
+                return null;
+        }
+    }
+    
+    public enum DaoTypes{
+        ITEM
+    }
+}
